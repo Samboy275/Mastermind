6bb15df454161f3685c3b11d1a41fb0891728cad
samual moahmed <sam-wal@windowslive.com>
2022-07-31 21:55:43 +0200

    add CodeBreaker and CodeMaker, Remove mastermind
    added player classes and removed mastermind class
    to make them handle the game

--- main.rb
+++ main.rb
@@ -7,53 +7,64 @@ module Game
   end
 end
 
-# class for the mastermind game
-class Mastermind
-  # declare availabe colors
-  COLORS = %w[blue yellow red green orange purple].freeze
-  # getters for instance objects
-  attr_reader :code, :COLORS, :rating
-  # instance variables
-  @rating  = ['','','','']
-  def initialize(player_code)
-    # take the code and and seperate it to make the code
-    @code = player_code.split('-').map do |item|
-      COLORS[item.to_i - 1]
+# class for the code maker
+class CodeMaker
+  # 1 - take code maker input to make a code out of a fixed amount of colors
+  # 2 - take in code breaker guess and rate it
+  # 3 - return an array rating the player guesses if all are perfect game ends
+  include Game
+  attr_reader :code
+
+  def initialize(code)
+    @code = code.split('-').map do |index|
+      Game::COLORS[index.to_i - 1]
     end
   end
 
-  # rate the codebreaker input
   def rate_guess(guess)
-    rating = ['', '','','']
-    availabe = [1,2,3,4]
-    code.each_with_index{ |color, index|
-      if guess.include? color
-        i = availabe.sample
-        availabe.delete(i)
-        if index == guess.index(color)
-          rating[i] = 'perfect'
-        else
-          rating[i] = 'exists'
-        end
-      end
-    }
+    rating = ['', '', '', '']
+    availabe = [1, 2, 3, 4]
+    code.each_with_index do |color, index|
+      next unless guess.include? color
+
+      i = availabe.sample
+      availabe.delete(i)
+      rating[i] = if index == guess.index(color)
+                    'perfect'
+                  else
+                    'exists'
+                  end
+    end
     rating
   end
+end
 
+# class for code breaker
+class CodeBreaker
+  # 1 - initialize a codebreaker object with number of tries
+  # 2 - make a guess function that takes and stores the breaker guess
+  attr_reader :tries, :guess
+
+  def initialize
+    @guess = ''
+    @tries = 6
+  end
+
+  def guess=(guess)
+    @guess = guess.split('-')
+  end
 end
 puts "make a 4 color code out of #{Game::COLORS.each_with_index.map do |color, index|
   "#{index + 1}- #{color}"
 end.join(' ')} using the colors numbers"
-
+print 'input the numbers in this pattern #-#-#-#: '
 player_code = ''
-player_code = gets.chomp until player_code.match?(/[0-9]\-[0-9]\-[0-9]\-[0-9]/)
-game = Mastermind.new(player_code)
+player_code = gets.chomp until player_code.match?(/[0-9]-[0-9]-[0-9]-[0-9]/)
+code_maker = CodeMaker.new(player_code)
 
-puts game.code
-
-
-puts 'Enter your guess seperated with spaces'
-
-guess = gets.chomp.split(' ')
-
-puts game.rate_guess(guess)
+puts code_maker.code
+print 'input the guess in this pattern color-color-color-color: '
+guess = ''
+guess = gets.chomp until guess.match?(/[a-z]+-[a-z]+-[a-z]+-[a-z]+/i)
+guess = guess.split('-')
+puts code_maker.rate_guess(guess)
